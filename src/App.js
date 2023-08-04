db7b0b25e918a1be4065c0f177305311849f2ee4
Simona <simonasablinaite@gmail.com>
2023-08-04 12:54:52 +0300

    Initialized repo

--- src/App.js
+++ src/App.js
@@ -1,22 +1,18 @@
-import logo from './logo.svg';
 import './App.css';
+import { Routes, Route } from 'react-router-dom';
+import { NavBar } from './Components/NavBar';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>HELLO</h1>
+        <NavBar />
+
+        <Routes>
+          <Route path='/' />
+          <Route path='/cart' />
+        </Routes>
       </header>
     </div>
   );
---
+++ src/Components/NavBar.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export const NavBar = () => {
+   return (
+      <div>
+         N
+      </div>
+   )
+}
+
+
